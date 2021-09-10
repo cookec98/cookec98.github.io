6758b68f6d794fb443318986b591da4dde4937a6
cookec98 <cookecampbell@gmail.com>
2021-09-10 18:09:40 -0500

    Added Win Streak Tracker and Adjusted Style

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,13 +1,17 @@
+var random = 2;
+var author = "";
+
 $(document).ready(function() {
   $('.quoteButton').click(function() {
-    var random = Math.floor(Math.random()*2);
+    random = Math.floor(Math.random()*2);
     console.log(random);
     if (random == 0){
       $.ajax({
         dataType: "json",
         url: "https://animechan.vercel.app/api/random",
         success: function(results) {
-          $('.quote').text(results["quote"] + " anime");
+          $('.quote').text(results["quote"]);
+          author = results["anime"];
         },
         error: function(xhr,status,error) {
           console.log(error);
@@ -19,7 +23,8 @@ $(document).ready(function() {
         dataType: "json",
         url: "https://api.quotable.io/random",
         success: function(results) {
-          $('.quote').text(results["content"] + " real");
+          $('.quote').text(results["content"]);
+          author = results["author"];
         },
         error: function(xhr,status,error) {
           console.log(error);
@@ -27,4 +32,23 @@ $(document).ready(function() {
       });
     }
   });
+});
+var winStreak = 0;
+$('.goku').on("click", function() {
+  if (random == 0){
+    winStreak ++;
+    $('.correct').text("Correct!! Quote from: " + author + ". Win streak is " + winStreak);
+  } else {
+    winStreak = 0
+    $('.correct').text("Incorrect D: Quote from: " + author + ". Win streak is " + winStreak);
+  }
+});
+$('.people').on("click", function() {
+  if (random != 0){
+    winStreak ++;
+    $('.correct').text("Correct!! Quote from: " + author + ". Win streak is " + winStreak);
+  } else {
+    winStreak = 0
+    $('.correct').text("Incorrect D: Quote from: " + author + ". Win streak is " + winStreak);
+  }
 });
